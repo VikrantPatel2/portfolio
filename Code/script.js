8e0dbbb1d928487927b665eae3fe3dd170974819
Vikrant Patel <vikrantpatel080@gmail.com>
2025-07-23 09:32:23 +0530

    commited successfully

---
+++ Code/script.js
@@ -0,0 +1,12 @@
+const changedata = [
+  "Frontend Developer",
+  "Seeking Backend Development",
+  "Traveller",
+  "Designer",
+];
+let i = 0;
+
+setInterval(() => {
+  document.getElementById("change").textContent = changedata[i];
+  i = (i + 1) % changedata.length;
+}, 1000);
